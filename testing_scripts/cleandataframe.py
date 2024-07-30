1a9fad94595abd1900160bb2ab4a93ab7c13fa52
ch <ch@hhs-MacBook-Air-3.local>
2024-07-30 14:22:12 -0700

    Reformatted files & libraries.

---
+++ testing_scripts/cleandataframe.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import re
+
+# Assign each entry in the filtered dataframe a label (0 for negative, 1 for positive)
+POSITIVE_LABEL, NEGATIVE_LABEL = 1, 0
+'''
+Given a row of the dataframe, returns 
+    1 if the entry belongs to the positive class
+    0 if the entry belongs to the negative class
+    NA if the entry is to be excluded
+Can be thought of as "h" (although this function does not operate on a feature vector)
+
+Currently, the positive class are entries where
+   1. The primary keyword is "Project manager" (case insensitive) AND  
+   2. The position contains "Project manager" (case insensitive),
+while the negative class are entries where
+   1. The primary keyword is "Java Developer" (case insensitive) AND  
+   2. The position contains "Java Developer" (case insensitive),
+
+Examine the effect of the second condition with
+   print(labeled_df.loc[ (labeled_df["True Label"] == NEGATIVE_LABEL) & (labeled_df["Primary Keyword"] == PM) ])
+'''
+
+def trueLabelFunction(row):
+    # print(f"id = {row['id']}, Position = {row['Position']}, Primary Keyword = {row['Primary Keyword']}")
+    
+    POSITIVE_POSITION = "Project Manager"
+    POSITIVE_KEYWORD = "Project Manager"
+
+    NEGATIVE_POSITION = "QA Engineer"   # "Java Developer"
+    NEGATIVE_KEYWORD = "QA"             # "Java"
+
+    # Positive match
+    positivePositionRegex = re.compile(f'.*{POSITIVE_POSITION}.*', re.IGNORECASE)
+    isPositivePositionMatch: bool = isinstance(row["Position"], str) and bool(positivePositionRegex.match(row["Position"]))
+    positivePrimaryKeywordRegex = re.compile(f'{POSITIVE_KEYWORD}', re.IGNORECASE)
+    isPositivePrimaryKeywordMatch: bool = isinstance(row["Primary Keyword"], str) and bool(positivePrimaryKeywordRegex.match(row["Position"]))
+
+    isPositiveMatch: bool = isPositivePositionMatch and isPositivePrimaryKeywordMatch
+
+    # Negative match
+    negativePositionRegex = re.compile(f'.*{NEGATIVE_POSITION}.*', re.IGNORECASE)
+    isNegativePositionMatch: bool = isinstance(row["Position"], str) and bool(negativePositionRegex.match(row["Position"]))
+    negativePrimaryKeywordRegex = re.compile(f'{NEGATIVE_KEYWORD}', re.IGNORECASE)
+    isNegativePrimaryKeywordMatch: bool = isinstance(row["Primary Keyword"], str) and bool(negativePrimaryKeywordRegex.match(row["Position"]))
+
+    isNegativeMatch: bool = isNegativePositionMatch and isNegativePrimaryKeywordMatch
+
+    if isPositiveMatch:
+        return POSITIVE_LABEL
+    elif isNegativeMatch:
+        return NEGATIVE_LABEL
+    else:
+        return pd.NA
+
+# Creates a true label column
+if __name__ == "__main__":
+    print("Labeling 1/0 Labels for Data.")
+    df = pd.read_parquet('data/resumes.parquet', engine='pyarrow')  # raw dataframe
+    # Filter the dataframe minimum cv length
+    MIN_CV_LENGTH = 500
+    filtered_df = df.loc[df['CV'].dropna().apply(len) >= MIN_CV_LENGTH]
+    labeled_df = filtered_df.copy()
+    labeled_df["True Label"] = labeled_df.apply(trueLabelFunction, axis=1)
+    labeled_df = labeled_df[labeled_df["True Label"].notna()]    # Filter out rows whose label value is NA
+    labeled_df.to_csv("Filtered_Truth_label.csv")   
---
+++ testing_scripts/score_resumes.py
@@ -0,0 +1,77 @@
+import numpy as np
+import yaml
+import os
+
+import pandas as pd
+#need to do:  conda install conda-forge::qdrant-client
+from qdrant_client import QdrantClient
+
+#Generate word similarity score for one pair: (job description & resume). Code from: https://github.com/srbhr/Resume-Matcher/blob/main/scripts/similarity/get_score.py
+def get_score(resume_string, job_description_string):
+    """
+    The function `get_score` uses QdrantClient to calculate the similarity score between a resume and a
+    job description.
+
+    Args:
+      resume_string: The `resume_string` parameter is a string containing the text of a resume. It
+    represents the content of a resume that you want to compare with a job description.
+      job_description_string: The `get_score` function you provided seems to be using a QdrantClient to
+    calculate the similarity score between a resume and a job description. The function takes in two
+    parameters: `resume_string` and `job_description_string`, where `resume_string` is the text content
+    of the resume and
+
+    Returns:
+      The function `get_score` returns the search result obtained by querying a QdrantClient with the
+    job description string against the resume string provided.
+    """
+   # logger.info("Started getting similarity score")
+
+    documents: List[str] = [resume_string]
+    client = QdrantClient(":memory:")
+    client.set_model("BAAI/bge-base-en")
+
+    client.add(
+        collection_name="demo_collection",
+        documents=documents,
+    )
+
+    search_result = client.query(
+        collection_name="demo_collection", query_text=job_description_string
+    )
+    #logger.info("Finished getting similarity score")
+    return search_result
+
+#Wrapper to perform get_score on a list of resumes (from data_frame labeled).
+def append_scores(dataframe_labeled, job_description, job_title):
+    #appends score of similarity of Job title to dataframe.
+    scores = [0 for i in range(dataframe_labeled)]
+    counter=0
+    for index, row in dataframe_labeled.iterrows():
+        resumm = "".join(row.to_dict()['CV'].split('\r\n')).split('\n')
+        score_result = get_score("".join(resumm), job_description)
+        similarity_score = round(score_result[0].score * 100, 2)
+        scores[counter] = similarity_score
+        print(similarity_score, row['Position'], flush=True)
+        counter+=1
+    dataframe_labeled[job_title+" Score"] = scores
+    return dataframe_labeled
+
+
+if __name__ == "__main__":
+    print("Scoring Data for resumes.", flush=True)
+    from cleandataframe import trueLabelFunction
+    df = pd.read_parquet('data/resumes.parquet', engine='pyarrow')  # raw dataframe
+    # Filter the dataframe minimum cv length
+    MIN_CV_LENGTH = 500
+    filtered_df = df.loc[df['CV'].dropna().apply(len) >= MIN_CV_LENGTH]
+    labeled_df = filtered_df.copy()
+    labeled_df["True Label"] = labeled_df.apply(trueLabelFunction, axis=1)
+    labeled_df = labeled_df[labeled_df["True Label"].notna()] 
+    job_doordash_pm = """
+    Tailor my resume to this job description and not 
+    make anything up: Product Manager (Multiple Levels) - 
+    Doordash: About the Team At DoorDash, we're redefining the future of on-demand delivery. 
+    To do this, we're building a world-class product organization, in which each of our product managers play a critical role in helping to define and execute our vision to connect local delivery networks in cities all across the world! About The Role Product Managers at DoorDash require a sharp consumer-first eye, platform thinking and strong cross-functional collaboration. As a Product Manager at DoorDash, you will own the product strategy and vision, define the product roadmap and alignment, and help drive the execution. You will be working on mission-critical products that shape the direction of the company. You will report into one of the following pillars: Merchant, Consumer, Operational Excellence, Ads, Logistics, or New Verticals. This role is a hybrid of remote work and in-person collaboration. You’re Excited About This Opportunity Because You Will… Drive the product definition, strategy, and long term vision. You own the roadmap Work closely with cross-functional teams of designers, operators, data scientists and engineers Communicate product plans, benefits and results to key stakeholders including leadership team We’re Excited About You Because… You have 5+ years of Product Management Industry Experience You have 4+ years of user-facing experience in industries such as eCommerce, technology or multi-sided marketplaces You have proven abilities in driving product strategy, vision, and roadmap alignment You’re an execution power-house You have experience presenting business reviews to senior executives You have empathy for the users you build for You are passionate about DoorDash and the problems we are solving for About DoorDash At DoorDash, our mission to empower local economies shapes how our team members move quickly, learn, and reiterate in order to make impactful decisions that display empathy for our range of users—from Dashers to merchant partners to consumers. We are a technology and logistics company that started with door-to-door delivery, and we are looking for team members who can help us go from a company that is known for delivering food to a company that people turn to for any and all goods. DoorDash is growing rapidly and changing constantly, which gives our team members the opportunity to share their unique perspectives, solve new challenges, and own their careers. We're committed to supporting employees’ happiness, healthiness, and overall well-being by providing comprehensive benefits and perks including premium healthcare, wellness expense reimbursement, paid parental leave and more. Our Commitment to Diversity and Inclusion We’re committed to growing and empowering a more inclusive community within our company, industry, and cities. That’s why we hire and cultivate diverse teams of people from all backgrounds, experiences, and perspectives. We believe that true innovation happens when everyone has room at the table and the tools, resources, and opportunity to excel. Statement of Non-Discrimination: In keeping with our beliefs and goals, no employee or applicant will face discrimination or harassment based on: race, color, ancestry, national origin, religion, age, gender, marital/domestic partner status, sexual orientation, gender identity or expression, disability status, or veteran status. Above and beyond discrimination and harassment based on 'protected categories,' we also strive to prevent other subtler forms of inappropriate behavior (i.e., stereotyping) from ever gaining a foothold in our office. Whether blatant or hidden, barriers to success have no place at DoorDash. We value a diverse workforce – people who identify as women, non-binary or gender non-conforming, LGBTQIA+, American Indian or Native Alaskan, Black or African American, Hispanic or Latinx, Native Hawaiian or Other Pacific Islander, differently-abled, caretakers and parents, and veterans are strongly encouraged to apply. Thank you to the Level Playing Field Institute for this statement of non-discrimination. Pursuant to the San Francisco Fair Chance Ordinance, Los Angeles Fair Chance Initiative for Hiring Ordinance, and any other state or local hiring regulations, we will consider for employment any qualified applicant, including those with arrest and conviction records, in a manner consistent with the applicable regulation. If you need any accommodations, please inform your recruiting contact upon initial connection."
+    """
+    append_scores(labeled_df, job_doordash_pm, "DoorDash PM")
+    labeled_df.to_csv("Scored_Resumes.csv")
