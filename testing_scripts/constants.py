80977be74f3e1e658d75ad9e918e934db5cb46e0
JackHsieh3 <154277593+JackHsieh3@users.noreply.github.com>
2024-08-08 14:39:54 -0700

    Add constants file
    To store constants that need to be referenced across all scripting files

---
+++ testing_scripts/constants.py
@@ -0,0 +1,2 @@
+POSITIVE_LABEL, NEGATIVE_LABEL = 1, 0       # Classification labels
+
--- testing_scripts/label_resumes.py
+++ testing_scripts/label_resumes.py
@@ -1,10 +1,9 @@
+from . import constants
 import pandas as pd
 import re
 
-# Assign each entry in the filtered dataframe a label (0 for negative, 1 for positive)
-POSITIVE_LABEL, NEGATIVE_LABEL = 1, 0
-
-def trueLabelFunction(row):
+# Assign each entry in the filtered dataframe a label (0 for negative, 1 for positive, NA for neither)
+def get_true_label(row, positive_position: str, positive_keyword: str, negative_position: str, negative_keyword: str):
     '''
     Given a row of the dataframe, returns 
         1 if the entry belongs to the positive class
@@ -23,36 +22,40 @@ def trueLabelFunction(row):
     print(labeled_df.loc[ (labeled_df["True Label"] == NEGATIVE_LABEL) & (labeled_df["Primary Keyword"] == PM) ])
     '''
 
-    # print(f"id = {row['id']}, Position = {row['Position']}, Primary Keyword = {row['Primary Keyword']}")
-    
-    POSITIVE_POSITION = "Project Manager"
-    POSITIVE_KEYWORD = "Project Manager"
-
-    NEGATIVE_POSITION = "QA Engineer"   # "Java Developer"
-    NEGATIVE_KEYWORD = "QA"             # "Java"
-
     # Positive match
-    positivePositionRegex = re.compile(f'.*{POSITIVE_POSITION}.*', re.IGNORECASE)
+    positivePositionRegex = re.compile(f'.*{positive_position}.*', re.IGNORECASE)
     isPositivePositionMatch: bool = isinstance(row["Position"], str) and bool(positivePositionRegex.match(row["Position"]))
-    positivePrimaryKeywordRegex = re.compile(f'{POSITIVE_KEYWORD}', re.IGNORECASE)
+    positivePrimaryKeywordRegex = re.compile(f'{positive_keyword}', re.IGNORECASE)
     isPositivePrimaryKeywordMatch: bool = isinstance(row["Primary Keyword"], str) and bool(positivePrimaryKeywordRegex.match(row["Position"]))
 
     isPositiveMatch: bool = isPositivePositionMatch and isPositivePrimaryKeywordMatch
 
     # Negative match
-    negativePositionRegex = re.compile(f'.*{NEGATIVE_POSITION}.*', re.IGNORECASE)
+    negativePositionRegex = re.compile(f'.*{negative_position}.*', re.IGNORECASE)
     isNegativePositionMatch: bool = isinstance(row["Position"], str) and bool(negativePositionRegex.match(row["Position"]))
-    negativePrimaryKeywordRegex = re.compile(f'{NEGATIVE_KEYWORD}', re.IGNORECASE)
+    negativePrimaryKeywordRegex = re.compile(f'{negative_keyword}', re.IGNORECASE)
     isNegativePrimaryKeywordMatch: bool = isinstance(row["Primary Keyword"], str) and bool(negativePrimaryKeywordRegex.match(row["Position"]))
 
     isNegativeMatch: bool = isNegativePositionMatch and isNegativePrimaryKeywordMatch
 
     if isPositiveMatch:
-        return POSITIVE_LABEL
+        return constants.POSITIVE_LABEL
     elif isNegativeMatch:
-        return NEGATIVE_LABEL
+        return constants.NEGATIVE_LABEL
     else:
         return pd.NA
+
+def add_true_label_column(df: pd.DataFrame, positive_position: str, positive_keyword: str, negative_position: str, negative_keyword: str):
+    '''
+    Adds a new column to the dataframe with the true label in place
+    '''
+    TRUE_LABEL_COLUMN_NAME = "True Label"
+
+    label = lambda resume : get_true_label(resume, positive_position, positive_keyword, negative_position, negative_keyword)
+    df[TRUE_LABEL_COLUMN_NAME] = df.apply(label, axis = 1)
+
+    return
+
 
 # Creates a true label column
 if __name__ == "__main__":
@@ -62,6 +65,6 @@ if __name__ == "__main__":
     MIN_CV_LENGTH = 500
     filtered_df = df.loc[df['CV'].dropna().apply(len) >= MIN_CV_LENGTH]
     labeled_df = filtered_df.copy()
-    labeled_df["True Label"] = labeled_df.apply(trueLabelFunction, axis=1)
+    labeled_df["True Label"] = labeled_df.apply(get_true_label, axis=1)
     labeled_df = labeled_df[labeled_df["True Label"].notna()]    # Filter out rows whose label value is NA
     labeled_df.to_csv("data/Filtered_Truth_label.csv")   
