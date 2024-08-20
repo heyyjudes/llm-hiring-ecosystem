f444014020cd575deceb6bf43a56152ddea8347d
JackHsieh3 <154277593+JackHsieh3@users.noreply.github.com>
2024-08-20 12:49:39 -0700

    Decompose label_resumes.py
    Add a matching function

--- testing_scripts/label_resumes.py
+++ testing_scripts/label_resumes.py
@@ -4,14 +4,43 @@ import re
 # from typing import Iterable
 from collections.abc import Iterable   # import directly from collections for Python < 3.3
 
+# Returns TRUE if a job matches a certain position and keywword
+# Leave slot as empty string to not check
+def is_match(row, positions: str | Iterable[str] = "", keywords: str | Iterable[str] = "") -> bool:
+    
+    if isinstance(positions, str):
+        position_string = positions
+    else:
+        position_string = "|".join(positions)
+
+    if isinstance(keywords, str):
+        keyword_string = keywords
+    else:
+        keyword_string = "|".join(keywords)
+
+    # Escape forward slash
+    # positive_position = positive_position.replace("/", "\/")
+    # positive_keyword = positive_keyword.replace("/", "\/")
+    # negative_position = negative_position.replace("/", "\/")
+    # negative_keyword = negative_keyword.replace("/", "\/")
+
+    position_regex = re.compile(f'.*({position_string}).*', re.IGNORECASE)
+    keyword_regex = re.compile(f'.*({keyword_string}).*', re.IGNORECASE)    # Searches for contaiment (in case of empty string)
+
+    position = row["Position"]
+    keyword = row["Primary Keyword"]
+    is_position_match: bool = isinstance(position, str) and bool(position_regex.match(position))
+    is_keyword_match: bool = isinstance(keyword, str) and bool(keyword_regex.match(keyword))
+
+    return is_position_match and is_keyword_match
+
+
 # Assign each entry in the filtered dataframe a label (0 for negative, 1 for positive, NA for neither)
 def get_true_label(row, positive_positions: str | Iterable[str], 
                         positive_keywords: str | Iterable[str], 
                         negative_positions: str | Iterable[str], 
                         negative_keywords: str | Iterable[str],
                         verbose: bool = False):
-# def get_true_label(row, positive_position: str, positive_keyword: str, negative_position: str, negative_keyword: str):
-
     '''
     Given a row of the dataframe, returns 
         1 if the entry belongs to the positive class
@@ -20,72 +49,25 @@ def get_true_label(row, positive_positions: str | Iterable[str],
     Can be thought of as "h" (although this function does not operate on a feature vector)
 
     Currently, the positive class are entries where
-    1. The primary keyword is "Project manager" (case insensitive) AND  
+    1. The primary keyword contains "Project manager" (case insensitive) AND  
     2. The position contains "Project manager" (case insensitive),
     while the negative class are entries where
-    1. The primary keyword is "Java Developer" (case insensitive) AND  
+    1. The primary keyword contains "Java Developer" (case insensitive) AND  
     2. The position contains "Java Developer" (case insensitive),
 
     Examine the effect of the second condition with
     print(labeled_df.loc[ (labeled_df["True Label"] == NEGATIVE_LABEL) & (labeled_df["Primary Keyword"] == PM) ])
     '''
 
-    # Join potentially multiple phrase to form a single phrase
-    if isinstance(positive_positions, str):
-        positive_position = positive_positions
-    else:
-        positive_position = "|".join(positive_positions)
+    # if verbose:
+    #     print(f"Positive position = {positive_position}")
+    #     print(f"Positive keyword = {positive_keyword}")
+    #     print(f"Negative position = {negative_position}")
+    #     print(f"Negative keyword = {negative_keyword}")
 
-    if isinstance(positive_keywords, str):
-        positive_keyword = positive_keywords
-    else:
-        positive_keyword = "|".join(positive_keywords)
-
-
-    if isinstance(negative_positions, str):
-        negative_position = negative_positions
-    else:
-        negative_position = "|".join(negative_positions)
-
-    if isinstance(negative_keywords, str):
-        negative_keyword = negative_keywords
-    else:
-        negative_keyword = "|".join(negative_keywords)
-
-    # Escape forward slash
-    # positive_position = positive_position.replace("/", "\/")
-    # positive_keyword = positive_keyword.replace("/", "\/")
-    # negative_position = negative_position.replace("/", "\/")
-    # negative_keyword = negative_keyword.replace("/", "\/")
-
-    if verbose:
-        print(f"Positive position = {positive_position}")
-        print(f"Positive keyword = {positive_keyword}")
-        print(f"Negative position = {negative_position}")
-        print(f"Negative keyword = {negative_keyword}")
-
-    # Positive match
-    positivePositionRegex = re.compile(f'.*({positive_position}).*', re.IGNORECASE)
-    positivePrimaryKeywordRegex = re.compile(f'({positive_keyword})', re.IGNORECASE)
-
-    position = row["Position"]
-    primaryKeyword = row["Primary Keyword"]
-
-    isPositivePositionMatch: bool = isinstance(position, str) and bool(positivePositionRegex.match(position))
-    isPositivePrimaryKeywordMatch: bool = isinstance(primaryKeyword, str) and bool(positivePrimaryKeywordRegex.match(primaryKeyword))
-    isPositiveMatch: bool = isPositivePositionMatch and isPositivePrimaryKeywordMatch
-
-    # Negative match
-    negativePositionRegex = re.compile(f'.*({negative_position}).*', re.IGNORECASE)
-    negativePrimaryKeywordRegex = re.compile(f'({negative_keyword})', re.IGNORECASE)
-
-    isNegativePositionMatch: bool = isinstance(position, str) and bool(negativePositionRegex.match(position))
-    isNegativePrimaryKeywordMatch: bool = isinstance(primaryKeyword, str) and bool(negativePrimaryKeywordRegex.match(primaryKeyword))
-    isNegativeMatch: bool = isNegativePositionMatch and isNegativePrimaryKeywordMatch
-
-    if isPositiveMatch:
+    if is_match(row, positive_positions, positive_keywords):
         return constants.POSITIVE_LABEL
-    elif isNegativeMatch:
+    elif is_match(row, negative_positions, negative_keywords):
         return constants.NEGATIVE_LABEL
     else:
         return pd.NA
