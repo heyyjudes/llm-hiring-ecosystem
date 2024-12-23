b0dad001b58c5687fa6822d5f6eb56a51ee4412c
Connie <33846658+chenmo8@users.noreply.github.com>
2024-12-23 14:25:06 -0800

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,33 @@
+MAX_TOKENS=1024
+
+scalable_pm_job_name = 'Scalable PM'
+scalable_pm_job_desc = '''Job Description
+This role is situated in a cross-functional team within a new product tribe. All teams in this tribe together are building a new greenfield technology platform, laying the foundation for an exciting set of future products and services. It’s a crucial element in Scalable’s long-term strategy.
+As a Product Manager of this team you will work alongside your engineering colleagues and contribute to this exciting new project. Additionally, you’ll frequently collaborate with other expert stakeholders inside and outside of Scalable Capital.
+Within the team you are responsible for product strategy and product roadmap and also drive the implementation of the best solutions to challenging problems
+You own the product development process end-to-end and coordinate all relevant stakeholders
+You lead the integrations with other Scalable Capital services as well as the connections to the complex and regulated capital markets infrastructure outside our perimeter
+You coordinate resources, create timelines and manage dependencies across teams to ensure best delivery
+You work alongside a highly motivated team of software engineers, product managers and academics implementing change in the financial industry
+Qualifications
+Passion for technology, finance and digital products
+Experience in agile product development, ideally in the capital markets or, banking domains
+Experience in building highly scalable & reliable cloud-native, microservice-based technology platforms
+Strong analytical thinking, leveraging both quantitative and qualitative data to drive decisions
+Utmost attention to detail and a culture of excellence
+Excellent communication skills that are clear, concise, and targeted towards your audience - software engineer, stakeholder or executive
+A great understanding of and focus on customer problems and needs
+Hands-on mentality: from ideation and research to prototype and user story, you can handle it
+Experience with common agile working tools like Jira, Confluence, Balsamiq, Figma and similar applications
+Excellent English communication skills (German is advantageous but not necessary)
+Additional Information
+Be part of one of the fastest-growing and most visible Fintech startups in Europe, creating innovative services that have a substantial impact on the lives of our customers
+Work with an international, diverse, inclusive, and ever-growing team that loves creating the best products for our clients
+Enjoy an office in a great location in the middle of Munich, Berlin, or choose to work remotely within Germany (if eligible for the job)
+Flexible vacation policy and the opportunity to work from abroad
+Be productive with the latest hardware and tools
+Learn and grow by joining our in-house knowledge sharing sessions and spending your individual Education Budget'''
+
+#Have templates with separate files
+
+#Have prompts with different templates -> 
