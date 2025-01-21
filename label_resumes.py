41ce5a0038eda33989f87fe09fd06cb22d475b82
ch <ch@DN0a2246a5.SUNet>
2025-01-21 12:36:08 -0800

    Sorted example input data, and added code to label resumes from input parquet file.

--- label_resumes.py
+++ label_resumes.py
@@ -112,4 +112,5 @@ if __name__ == "__main__":
     labeled_df = filtered_df.copy()
     labeled_df["True Label"] = labeled_df.apply(get_true_label, axis=1)
     labeled_df = labeled_df[labeled_df["True Label"].notna()]    # Filter out rows whose label value is NA
+    
     labeled_df.to_csv("data/Filtered_Truth_label.csv")   
